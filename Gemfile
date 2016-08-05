e2bbbb1486067904d95a1e51cacbe875455eb564
fifiteen82726 <fifiteen82726@gmail.com>
2016-08-06 02:19:24 +0800

    add swaager-ui

--- Gemfile
+++ Gemfile
@@ -22,11 +22,11 @@ gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0', group: :doc
-
 gem 'grape'
 gem 'hashie-forbidden_attributes'
 
-
+gem 'grape-swagger'
+gem 'grape-entity'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- app/api/fruit_api/api.rb
+++ app/api/fruit_api/api.rb
@@ -4,27 +4,31 @@ require 'grape'
 
 module FruitAPI
   class API < Grape::API
+    
     version 'v1', using: :header, vendor: 'fruit_api'
     format :json
     prefix :api
     content_type :json, 'application/json;charset=UTF-8'
     resource :nutrition do
-      desc 'Return a nutrition of fruit.'
-      get :public_timeline do
-        # Status.limit(20)
-
-        {:say => 'hahaha'}
+      desc 'Return a fruit.'
+      get :haha do
+        Fruit.find_by_name(params[:name])
       end
-
-    desc 'Return a fruit.'
-      # params do
-      #   requires :id, type: Integer, desc: 'Status id.'
-      # end
-      # route_param :id do
-        get :haha do
-          Fruit.find_by_name(params[:name])
-        end
-      # end
     end
+
+    add_swagger_documentation(
+      basd_path: "/v1",  #Swagger的base url
+
+      mount_path: 'doc', #代表我們要去 v1/doc
+
+      info: {
+        title: 'Fruit API', 
+        description: "A description of the API."
+        },
+        hide_documentation_path: true,
+        hide_format: true,
+        include_base_url: true
+        )
+
   end
 end
