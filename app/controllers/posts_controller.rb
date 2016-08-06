28e60c2e2c39fc95eccb84e6ba3f84950b7d4a94
fifiteen82726 <fifiteen82726@gmail.com>
2016-08-07 01:58:26 +0800

    finish 照片上傳 show

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -35,6 +35,7 @@ class PostsController < ApplicationController
 
     respond_to do |format|
       if @post.save
+        current_user.posts << @post
         format.html { redirect_to @post, notice: 'Post was successfully created.' }
         format.json { render :show, status: :created, location: @post }
       else
@@ -76,6 +77,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:content)
+      params.require(:post).permit(:content,:photo)
     end
 end
