21cd42e1cb8cf1f2afaa7bedc49bfad7376ba932
fifiteen82726 <fifiteen82726@gmail.com>
2016-08-07 22:38:31 +0800

    test

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ActiveRecord::Base
 	belongs_to :user
 	has_many :fruits
-	accepts_nested_attributes_for :furits
+	accepts_nested_attributes_for :fruits
 	has_attached_file :photo, styles: { large: "600x600>", medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
 	validates_attachment_content_type :photo, content_type: /\Aimage\/.*\Z/
 end
