d87718a9a1c420bac70a4df392151b8d977a71a3
fifiteen82726 <fifiteen82726@gmail.com>
2017-01-02 15:07:03 +0800

    update jquery ui, fix freelancer js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,22 +1,10 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file.
-//
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
-// about supported directives.
-//
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require_tree .
 //= require bootstrap-datepicker
 //= require bootstrap-sprockets
 //= require js/jquery.easing.min
 //= require js/jqBootstrapValidation
 //= require js/contact_me
 //= require js/freelancer.min
+//= require jquery-ui
--- app/assets/javascripts/js/freelancer.js
+++ app/assets/javascripts/js/freelancer.js
@@ -12,11 +12,6 @@
         event.preventDefault();
     });
 
-    // Highlight the top nav as scrolling occurs
-    $('body').scrollspy({
-        target: '.navbar-fixed-top',
-        offset: 51
-    });
 
     // Closes the Responsive Menu on Menu Item Click
     $('.navbar-collapse ul li a:not(.dropdown-toggle)').click(function() {
